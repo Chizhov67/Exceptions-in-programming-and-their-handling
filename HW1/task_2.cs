21e96b4814a120956c3978ec3077cc546644c517
Andrey <chizhov67@gmail.com>
2023-04-24 19:23:13 +0300

    task_2

---
+++ HW1/task_2.cs
@@ -0,0 +1,24 @@
+int[] DifferenceElse(int[] arr1, int[] arr2) 
+{
+    int[] ResArray = new int[arr1.Length];
+    try
+    {
+        if (arr1.Length != arr2.Length)
+        {
+            throw new Exception("Длины массивов различны!");
+        }
+        else
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                ResArray[i] = arr1[i] - arr2[i];
+            }
+            Print(ResArray);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Ошибка: {e.Message}");
+    }
+    return ResArray;
+}
